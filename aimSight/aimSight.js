db41a818df04bfceddf8ebd8298bcaca542adf53
Jeffery Patterson <jpatterson933@ucla.edu>
2022-05-11 19:52:18 -0700

    added two separate canvases. One as a type of aim sight using ccanvas. This can be used as an overaly. Another as a type of screen saver background

--- aimSight/aimSight.js
+++ aimSight/aimSight.js
@@ -5,7 +5,9 @@ let flowField;
 let flowFieldAnimation;
 
 window.onload = function () {
+    // we grab our html content where our canvas will display
     canvas = document.getElementById('canvas1');
+    // sets our canvas to 2d
     ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -38,50 +40,22 @@ class FlowFieldEffect {
     #height;
     constructor(ctx, width, height) {
         this.#ctx = ctx;
-        // this.#ctx.strokeStyle = 'white';
-        this.#ctx.lineWidth = 1;
+        this.#ctx.strokeStyle = 'white';
+        this.#ctx.lineWidth = 5;
         this.#width = width;
         this.#height = height;
         // this.angle = 0;
         this.lastTime = 0;
         this.interval = 1000/60;
         this.timer = 0;
-        // grid cell size
-        this.cellSize = 15;
-        this.gradient;
-        this.#createGradient();
-        this.#ctx.strokeStyle = this.gradient;
-        this.radius = 0;
-        // velocity of radius
-        this.vr = 0.003;
+        this.cellSize = 20;
 
     }
-
-    #createGradient() {
-        this.gradient = this.#ctx.createLinearGradient(0, 0, this.#width, this.#height);
-        this.gradient.addColorStop("0.1", "#ff5c33");
-        this.gradient.addColorStop("0.2", "#ff66b3");
-        this.gradient.addColorStop("0.4", "#ccccff");
-        this.gradient.addColorStop("0.6", "#b3ffff");
-        this.gradient.addColorStop("0.8", "#80ff80");
-        this.gradient.addColorStop("0.9", "#ffff33");
-
-
-    }
-
-    #drawLine(angle, x, y) {
-        let positionX = x;
-        let positionY = y;
-        let dx = mouse.x - positionX;
-        let dy = mouse.y - positionY;
-        let distance = dx * dx + dy * dy;
-        if (distance > 600000) distance = 600000;
-        else if (distance < 50000) distance = 50000;
-        const length = distance/10000;
+    #drawLine(x, y) {
+        const length = 300;
         this.#ctx.beginPath();
         this.#ctx.moveTo(x, y);
-        // x and y line movement / size on grid - direction of lines
-        this.#ctx.lineTo(x + Math.cos(angle) * length, y + Math.sin(angle) * length);
+        this.#ctx.lineTo(mouse.x, mouse.y);
         this.#ctx.stroke();
     }
 
@@ -91,17 +65,18 @@ class FlowFieldEffect {
         if(this.timer > this.interval) {
             // this.angle += 0.05;
             this.#ctx.clearRect(0, 0, this.#width, this.#height);
-            this.radius += this.vr;
-            if (this.radius > 10 || this.radius < -10) this.vr *= -1;
-            // this is how you map a grid on canvas using nested for loops
+
             for (let y = 0; y < this.#height; y += this.cellSize) {
                 for (let x = 0; x < this.#width; x += this.cellSize){
-                    // changing the 0.01 changes the zoomed in with the lower number zooming us in further
-                    const angle = (Math.cos(x * 0.5) + Math.sin(y * 0.5)) * this.radius;
-                    this.#drawLine(angle, x, y);
+                    this.#drawLine(x, y);
+
                 }
+
             }
+
+
             this.timer = 0;
+
         } else {
             this.timer += deltaTime;
         }
